6a1d8259070bdcdd95c6a744c10676d5c16a7071
Piero <piero.vasquez.r@gmail.com>
2020-02-19 21:45:12 +0100

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './components/recipes/recipes-detail/recipes-detail.component';
+import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping/shopping-edit/shopping-edit.component';
+import { RecipesItemComponent } from './components/recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.scss']
+})
+export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'test', 'https://okdiario.com/img/2019/07/09/pollo-asado-con-brandy-655x368.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
