c0118d7acd5fb47e7b9c9a2f16a59712cd9d2f18
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-06-08 14:47:25 +0200

    using keyframes in animations

--- src/app/components/shopping/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping/shopping-list/shopping-list.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as ShoppingListActions from './store/shopping-list.actions';
 import * as fromApp from '../../../core/store/app.reducer';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -62,6 +62,44 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         }),
         animate(300)
       ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *',
+        animate(1000, keyframes([
+          // Todos estos estilos cogeran una parte igual de los 1000 milisegundos
+          // con el offset, indicamos en que punto del milisegundo quieres que se ejecute el style
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0,
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ),
       transition('* => void', [
         animate(300, style({
           transform: 'translateX(100px)',
